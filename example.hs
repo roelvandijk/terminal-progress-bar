4e8e19a1e2a67bcee635836b7a97eac1f2dde983
Roel van Dijk <roel@lambdacube.nl>
2016-09-08 11:08:08 +0200

    Maintenance
    - Changed version from 0.0.1.4 to 0.1.0
    - Updated copyright
    - Bumps HUnit dependency (fixes #11)
    - Uses stderr by default (fixes #8)
    - Added hProgressBar function that prints to any file handle
    - Calls hFlush after outputting (fixes #9)
    - Uses prelude, fixing Data.String issue (see #6)
    - Dropped unicode formatting (easier to make contributions)
    - Added nix expressions for easy development
    - Updated cabal version to >=1.10
    - Re-enabled to example section, haddock works fine

--- example.hs
+++ example.hs
@@ -1,36 +1,28 @@
-{-# LANGUAGE NoImplicitPrelude, PackageImports, UnicodeSyntax #-}
+{-# LANGUAGE PackageImports #-}
 
 module Main where
 
 import "base" Control.Concurrent ( threadDelay )
 import "base" Control.Monad ( forM_ )
-import "base" Data.Function ( ($) )
-import "base" Data.Int ( Int )
-import "base" Prelude ( (+) )
-import "base" System.IO ( IO, hSetBuffering, BufferMode(NoBuffering), stdout, putStrLn )
-import "base-unicode-symbols" Prelude.Unicode ( ℤ )
-import "terminal-progress-bar" System.ProgressBar ( progressBar, percentage, exact, startProgress, incProgress )
+import "terminal-progress-bar" System.ProgressBar
+    ( progressBar, percentage, exact, startProgress, incProgress )
 
-
-main ∷ IO ()
+main :: IO ()
 main = do
-  example 60 (13 + 60) 25000
+  example  60 (13 + 60) 25000
   example' 60 (13 + 60) 25000
 
-example ∷ ℤ → ℤ → Int → IO ()
+example :: Integer -> Integer -> Int -> IO ()
 example t w delay = do
-    hSetBuffering stdout NoBuffering
-    forM_ [1..t] $ \d → do
+    forM_ [1..t] $ \d -> do
       progressBar percentage exact w d t
       threadDelay delay
     putStrLn ""
 
-example' ∷ ℤ → ℤ → Int → IO ()
+example' :: Integer -> Integer -> Int -> IO ()
 example' t w delay = do
-    hSetBuffering stdout NoBuffering
     (pr, _) <- startProgress percentage exact w t
-    forM_ [1..t] $ \d → do
+    forM_ [1..t] $ \_d -> do
       incProgress pr 1
       threadDelay delay
     putStrLn ""
-
--- src/System/ProgressBar.hs
+++ src/System/ProgressBar.hs
@@ -1,8 +1,10 @@
-{-# LANGUAGE NoImplicitPrelude, PackageImports, NamedFieldPuns, RecordWildCards, UnicodeSyntax #-}
+{-# language PackageImports, NamedFieldPuns, RecordWildCards #-}
 
 module System.ProgressBar
     ( -- * Progress bars
-      progressBar
+      ProgressBar
+    , progressBar
+    , hProgressBar
     , mkProgressBar
       -- * Labels
     , Label
@@ -10,77 +12,69 @@ module System.ProgressBar
     , msg
     , percentage
     , exact
-    -- * auto-printing
+      -- * Auto printing
     , ProgressRef
     , startProgress
     , incProgress
     ) where
 
-import "base" Control.Monad ( (=<<), (>>), return, when )
-import "base" Data.Bool     ( otherwise )
-import "base" Data.Function ( ($), (.) )
-import "base" Data.List     ( null, length, genericLength, genericReplicate )
-import "base" Data.Maybe    ( maybe )
-import "base" Data.Ord      ( min, max, (>=) )
+import "base" Control.Monad ( when )
+import "base" Data.List     ( genericLength, genericReplicate )
 import "base" Data.Ratio    ( (%) )
-import "base" Data.String   ( String )
-import "base" Prelude       ( (+), (-), round, floor, Bool(..) )
-import "base" System.IO     ( IO, putStr, putChar )
+import "base" System.IO     ( Handle, stderr, hPutChar, hPutStr, hFlush )
 import "base" Text.Printf   ( printf )
-import "base" Text.Show     ( show )
-import "base"  Control.Concurrent ( ThreadId, forkIO )
-import "stm"  Control.Concurrent.STM ( TVar, readTVar, writeTVar, newTVar, atomically, STM )
-import "stm-chans"  Control.Concurrent.STM.TMQueue ( TMQueue, readTMQueue, closeTMQueue, writeTMQueue, newTMQueue )
-import "base-unicode-symbols" Data.Bool.Unicode ( (∧) )
-import "base-unicode-symbols" Data.Eq.Unicode   ( (≢) )
-import "base-unicode-symbols" Prelude.Unicode   ( ℤ, ℚ, (⋅) )
+import "base" Control.Concurrent ( ThreadId, forkIO )
+import "stm"  Control.Concurrent.STM
+    ( TVar, readTVar, writeTVar, newTVar, atomically, STM )
+import "stm-chans"  Control.Concurrent.STM.TMQueue
+    ( TMQueue, readTMQueue, closeTMQueue, writeTMQueue, newTMQueue )
 
+-- | Type of functions producing a progress bar.
+type ProgressBar a
+   = Label   -- ^ Prefixed label.
+  -> Label   -- ^ Postfixed label.
+  -> Integer -- ^ Total progress bar width in characters.
+  -> Integer -- ^ Amount of work completed.
+  -> Integer -- ^ Total amount of work.
+  -> a
 
--- | Print a progress bar
+-- | Print a progress bar to 'stderr'
+--
+-- See 'hProgressBar'.
+progressBar :: ProgressBar (IO ())
+progressBar = hProgressBar stderr
+
+-- | Print a progress bar to a file handle.
 --
 -- Erases the current line! (by outputting '\r') Does not print a
 -- newline '\n'. Subsequent invocations will overwrite the previous
 -- output.
---
--- Remember to set the correct buffering mode for stdout:
---
--- > import System.IO ( hSetBuffering, BufferMode(NoBuffering), stdout )
--- > hSetBuffering stdout NoBuffering
-progressBar ∷ Label -- ^ Prefixed label.
-            → Label -- ^ Postfixed label.
-            → ℤ     -- ^ Total progress bar width in characters.
-            → ℤ     -- ^ Amount of work completed.
-            → ℤ     -- ^ Total amount of work.
-            → IO ()
-progressBar mkPreLabel mkPostLabel width todo done = do
-    putChar '\r'
-    putStr $ mkProgressBar mkPreLabel mkPostLabel width todo done
+hProgressBar :: Handle -> ProgressBar (IO ())
+hProgressBar hndl mkPreLabel mkPostLabel width todo done = do
+    hPutChar hndl '\r'
+    hPutStr hndl $ mkProgressBar mkPreLabel mkPostLabel width todo done
+    hFlush hndl
 
 -- | Renders a progress bar
 --
 -- >>> mkProgressBar (msg "Working") percentage 40 30 100
 -- "Working [=======>.................]  30%"
-mkProgressBar ∷ Label -- ^ Prefixed label.
-              → Label -- ^ Postfixed label.
-              → ℤ     -- ^ Total progress bar width in characters.
-              → ℤ     -- ^ Amount of work completed.
-              → ℤ     -- ^ Total amount of work.
-              → String
+mkProgressBar :: ProgressBar String
 mkProgressBar mkPreLabel mkPostLabel width todo done =
     printf "%s%s[%s%s%s]%s%s"
            preLabel
            prePad
            (genericReplicate completed '=')
-           (if remaining ≢ 0 ∧ completed ≢ 0 then ">" else "")
-           (genericReplicate (remaining - if completed ≢ 0 then 1 else 0)
+           (if remaining /= 0 && completed /= 0 then ">" else "")
+           (genericReplicate (remaining - if completed /= 0 then 1 else 0)
                              '.'
            )
            postPad
            postLabel
   where
     -- Amount of work completed.
-    fraction ∷ ℚ
-    fraction | done ≢ 0  = todo % done
+    fraction :: Rational
+    fraction | done /= 0  = todo % done
              | otherwise = 0 % 1
 
     -- Amount of characters available to visualize the progress.
@@ -93,41 +87,44 @@ mkProgressBar mkPreLabel mkPostLabel width todo done =
     -- Number of characters needed to represent the amount of work
     -- that is completed. Note that this can not always be represented
     -- by an integer.
-    numCompletedChars ∷ ℚ
-    numCompletedChars = fraction ⋅ (effectiveWidth % 1)
+    numCompletedChars :: Rational
+    numCompletedChars = fraction * (effectiveWidth % 1)
 
-    completed, remaining ∷ ℤ
+    completed, remaining :: Integer
     completed = min effectiveWidth $ floor numCompletedChars
     remaining = effectiveWidth - completed
 
-    preLabel, postLabel ∷ String
+    preLabel, postLabel :: String
     preLabel  = mkPreLabel  todo done
     postLabel = mkPostLabel todo done
 
-    prePad, postPad ∷ String
+    prePad, postPad :: String
     prePad  = pad preLabel
     postPad = pad postLabel
 
-    pad ∷ String → String
+    pad :: String -> String
     pad s | null s    = ""
           | otherwise = " "
 
 
 -- | A label that can be pre- or postfixed to a progress bar.
-type Label = ℤ → ℤ → String
+type Label
+   = Integer -- ^ Completed amount of work.
+  -> Integer -- ^ Total amount of work.
+  -> String  -- ^ Resulting label.
 
 -- | The empty label.
 --
 -- >>> noLabel 30 100
 -- ""
-noLabel ∷ Label
+noLabel :: Label
 noLabel = msg ""
 
 -- | A label consisting of a static string.
 --
 -- >>> msg "foo" 30 100
 -- "foo"
-msg ∷ String → Label
+msg :: String -> Label
 msg s _ _ = s
 
 -- | A label which displays the progress as a percentage.
@@ -138,9 +135,9 @@ msg s _ _ = s
 -- >>> percentage 30 100
 -- " 30%"
 
--- ∀ d t : ℕ. d ≤ t → length (percentage d t) ≡ 3
-percentage ∷ Label
-percentage done todo = printf "%3i%%" (round (done % todo ⋅ 100) ∷ ℤ)
+-- ∀ d t : ℕ. d ≤ t -> length (percentage d t) ≡ 3
+percentage :: Label
+percentage done todo = printf "%3i%%" (round (done % todo * 100) :: Integer)
 
 -- | A label which displays the progress as a fraction of the total
 -- amount of work.
@@ -151,28 +148,32 @@ percentage done todo = printf "%3i%%" (round (done % todo ⋅ 100) ∷ ℤ)
 -- >>> exact 30 100
 -- " 30/100"
 
--- ∀ d₁ d₂ t : ℕ. d₁ ≤ d₂ ≤ t → length (exact d₁ t) ≡ length (exact d₂ t)
-exact ∷ Label
+-- ∀ d₁ d₂ t : ℕ. d₁ ≤ d₂ ≤ t -> length (exact d₁ t) ≡ length (exact d₂ t)
+exact :: Label
 exact done total = printf "%*i/%s" (length totalStr) done totalStr
   where
     totalStr = show total
 
 -- * Auto-Printing Progress
 
-data ProgressRef = ProgressRef { prPrefix    ∷ Label
-                               , prPostfix   ∷ Label
-                               , prWidth     ∷ ℤ
-                               , prCompleted ∷ TVar ℤ
-                               , prTotal     ∷ ℤ
-                               , prQueue     ∷ TMQueue ℤ }
+data ProgressRef
+   = ProgressRef
+     { prPrefix    :: Label
+     , prPostfix   :: Label
+     , prWidth     :: Integer
+     , prCompleted :: TVar Integer
+     , prTotal     :: Integer
+     , prQueue     :: TMQueue Integer
+     }
 
 -- | Start a thread to automatically display progress. Use incProgress to step
 -- the progress bar.
-startProgress ∷ Label -- ^ Prefixed label.
-              → Label -- ^ Postfixed label.
-              → ℤ     -- ^ Total progress bar width in characters.
-              → ℤ     -- ^ Total amount of work.
-              → IO (ProgressRef, ThreadId)
+startProgress
+    :: Label   -- ^ Prefixed label.
+    -> Label   -- ^ Postfixed label.
+    -> Integer -- ^ Total progress bar width in characters.
+    -> Integer -- ^ Total amount of work.
+    -> IO (ProgressRef, ThreadId)
 startProgress mkPreLabel mkPostLabel width total = do
     pr  <- buildProgressRef
     tid <- forkIO $ reportProgress pr
@@ -184,36 +185,32 @@ startProgress mkPreLabel mkPostLabel width total = do
         return $ ProgressRef mkPreLabel mkPostLabel width completed total queue
 
 -- | Increment the progress bar. Negative values will reverse the progress.
--- Progress will never be negative and will silently stop taking data when it
--- completes.
-incProgress ∷ ProgressRef
-            → ℤ
-            → IO ()
-incProgress ProgressRef {prQueue} = atomically . writeTMQueue prQueue
+-- Progress will never be negative and will silently stop taking data
+-- when it completes.
+incProgress :: ProgressRef -> Integer -> IO ()
+incProgress progressRef =
+    atomically . writeTMQueue (prQueue progressRef)
 
-reportProgress ∷ ProgressRef
-               → IO ()
+reportProgress :: ProgressRef -> IO ()
 reportProgress pr = do
-  continue <- atomically $ updateProgress pr
-  renderProgress pr
-  when continue $ reportProgress pr
+    continue <- atomically $ updateProgress pr
+    renderProgress pr
+    when continue $ reportProgress pr
 
-updateProgress ∷ ProgressRef
-               → STM Bool
+updateProgress :: ProgressRef -> STM Bool
 updateProgress ProgressRef {prCompleted, prQueue, prTotal} = do
-  maybe dontContinue doUpdate =<< readTMQueue prQueue
-  where
-    dontContinue = return False
-    doUpdate countDiff = do
-      count <- readTVar prCompleted
-      let newCount = min prTotal $ max 0 $ count + countDiff
-      writeTVar prCompleted newCount
-      if newCount >= prTotal
-        then closeTMQueue prQueue >> dontContinue
-        else return True
-                       
-renderProgress ∷ ProgressRef
-               → IO ()
+    maybe dontContinue doUpdate =<< readTMQueue prQueue
+    where
+      dontContinue = return False
+      doUpdate countDiff = do
+        count <- readTVar prCompleted
+        let newCount = min prTotal $ max 0 $ count + countDiff
+        writeTVar prCompleted newCount
+        if newCount >= prTotal
+          then closeTMQueue prQueue >> dontContinue
+          else return True
+
+renderProgress :: ProgressRef -> IO ()
 renderProgress ProgressRef {..} = do
-  completed <- atomically $ readTVar prCompleted
-  progressBar prPrefix prPostfix prWidth completed prTotal
+    completed <- atomically $ readTVar prCompleted
+    progressBar prPrefix prPostfix prWidth completed prTotal
