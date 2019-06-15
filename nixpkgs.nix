87f42dc612c701e9e61aa628b64d1873828c40b8
Roel van Dijk <roel@lambdacube.nl>
2019-06-15 15:54:55 +0200

    Update nixpkgs to 19.03

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 # To calculate sha256:
 # > nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/3fd87ad0073fd1ef71a8fcd1a1d1a89392c33d0a.tar.gz
 builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/3fd87ad0073fd1ef71a8fcd1a1d1a89392c33d0a.tar.gz";
-  sha256 = "0n4ffwwfdybphx1iyqz1p7npk8w4n78f8jr5nq8ldnx2amrkfwhl";
+  url = "https://github.com/NixOS/nixpkgs/archive/50d5d73e22bb2830f490e26a528579facfc7f302.tar.gz";
+  sha256 = "0c1inf0pc2jizkrfl3629s154r55ya5asmwnwn6g64ppz2wwzizi";
 }
